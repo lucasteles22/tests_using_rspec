f2cf3b557b32c981587c26ee545c30be2145b15f
Lucas Teles <lucasteles22@gmail.com>
2016-04-06 17:19:18 -0300

    Add home controller with one action (index)

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "posts/new", type: :view do
-  before(:each) do
-    assign(:post, Post.new())
-  end
+  context "Create a new post" do
+    let(:post) { create(:post) }
+    let(:user) { create(:user, :email => 'mycustompostuser@test.com') }
 
-  it "renders new post form" do
-    render
+    before(:all) { 1.times {  create(:user, :email => 'mycustompostuser2@test.com') } }
+    after(:all) { User.delete_all }
 
-    assert_select "form[action=?][method=?]", posts_path, "post" do
+    scenario "has not title" do
+      visit '/posts/new'
+      within("#post_form") do
+        fill_in 'post_title', :with => post.title
+        fill_in 'post_body', :with => post.body
+        check 'post_approved'
+
+        # select User.first.id, :from => "post_author_id"
+        find('#post_author_id').find(:xpath, 'option[1]').select_option
+      end
+      click_button 'Create Post'
+      expect(page).to have_content 'Post was successfully created'
     end
   end
 end
